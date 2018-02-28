c234f72a3c43042e53e4019085a4b70740661b53
Adam Scibior <ams240@cam.ac.uk>
2018-02-28 14:43:17 +0000

    Add further signatures

--- src/Monad.ml
+++ src/Monad.ml
@@ -3,3 +3,30 @@ module type Monad = sig
   val return : 'a -> 'a t
   val (>>=) : 'a t -> ('a -> 'b t) -> 'b t
 end
+
+module type Sample = sig
+  type 'a t
+  val random : float t
+end
+
+module type Score = sig
+  type 'a t
+  val score : float -> unit t
+end
+
+module type MonadBayes = sig
+  type 'a t
+  val return : 'a -> 'a t
+  val (>>=) : 'a t -> ('a -> 'b t) -> 'b t
+  val random : float t
+  val score : float -> unit t
+end
+
+module Model (M : MonadBayes) =
+struct
+  open M
+  let model =
+    random >>= fun x ->
+    score 0.5 >>= fun _ ->
+    return (x < 0.5)
+end
